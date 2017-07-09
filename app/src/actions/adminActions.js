43aed0d37affe55efb9980ff7dd7136ec20626ec
edemtech <edemtechwork@gmail.com>
2017-07-09 22:32:34 +0300

    Get data started..

--- app/src/actions/adminActions.js
+++ app/src/actions/adminActions.js
@@ -3,7 +3,8 @@ import { apiPrefix, serverPort } from '../../etc/config.json';
 const ip = apiPrefix + serverPort;
 
 export function getData() {
-  return dispatch => {
-    return axios.get(ip+'/api/users');
-  };
+  // return dispatch => {
+  //   return axios.get(ip+'/api/users');
+  // };
+  return axios.get(ip+'/api/users');
 }
--- app/src/components/admin/AdminPage.js
+++ app/src/components/admin/AdminPage.js
@@ -1,37 +1,56 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { getData } from '../../actions/adminActions.js'
 
 class AdminPage extends React.Component{
+	constructor(props) {
+    super(props);
+		this.state = {
+      tableData: []
+    }
+		this.getRecords = this.getRecords.bind(this);
+  }
+
+	getRecords() {
+		getData().then(
+			(res) => { this.setState({tableData: res.data.data}) },
+			(err) => console.log('bad request'),
+		);
+	}
+
 	render(){
 		return(
-			<table className="table table-striped table-hover">
-				<thead>
-					<tr>
-						<th>Firstname</th>
-						<th>Lastname</th>
-						<th>Email</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>John</td>
-						<td>Doe</td>
-						<td>john@example.com</td>
-					</tr>
-					<tr>
-						<td>Mary</td>
-						<td>Moe</td>
-						<td>mary@example.com</td>
-					</tr>
-					<tr>
-						<td>July</td>
-						<td>Dooley</td>
-						<td>july@example.com</td>
-					</tr>
-				</tbody>
-			</table>
+			<div>
+				<button className="btn btn-success" onClick={this.getRecords}>Click some</button>
+				<table className="table table-striped table-hover">
+					<thead>
+						<tr>
+							<th>Username</th>
+							<th>email</th>
+							<th>permission</th>
+						</tr>
+					</thead>
+					<tbody>
+						{
+							this.state.tableData.map( item =>
+								<tr>
+									<td>{item.username}</td>
+									<td>{item.email}</td>
+									<td>{item.permission}</td>
+								</tr>
+							)
+						}
+					</tbody>
+				</table>
+			</div>
 		);
 	}
 }
 
-export default AdminPage;
+// export default AdminPage;
+function someProps(state) {
+  return {
+    auth: state.auth
+  };
+}
+export default connect(someProps)(AdminPage);
